87f5c08476dfe0645a543ac6a2492daafefe0ced
Philippine Lorrain <philippinelorrain@gmail.com>
2022-12-05 14:20:59 -0500

    auth-context: basically implemented login logic into the component

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  // line below: dummy func for better i.d.e autocompletion
   onLogout: () => {},
+  onLogin: (email, password) => {},
+
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
 export default AuthContext;
